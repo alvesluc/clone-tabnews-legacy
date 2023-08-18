f93c094153d86335d718c3c13fff619d2401d0c1
Lucas Alves <l.alves001@gmail.com>
2023-08-18 11:36:07 -0300

    Checking if GitHub Actions env are being set

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Image from "next/image";
 
 function Home() {
+  const isGitHubActions = process.env.GITHUB_ACTIONS || false;
+  const repository = isGitHubActions ? process.env.GITHUB_ACTIONS.replace(/.*?\//, "") : 'local';
+
   return (
     <>
-      <h2>Teste</h2>
+      <h2>Environment info</h2>
+      <p>Is GitHub: {`${isGitHubActions}`}</p>
+      <p>Repository: {repository}</p>
       <div>
         <Image
           fill
           src="/images/file-based-routing.png"
-          style={{objectFit: 'contain'}}
+          style={{ objectFit: "contain" }}
         />
       </div>
     </>
