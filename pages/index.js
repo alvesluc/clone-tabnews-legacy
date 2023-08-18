f68f3341f0995471a6baa11b36e220e5dee45c2d
Lucas Alves <l.alves001@gmail.com>
2023-08-18 12:05:55 -0300

    Removed unused images

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,6 @@ function Home() {
   return (
     <>
       <h2>Home</h2>
-      <img
-        width="100%"
-        src="../images/file-based-routing.png"
-        style={{ objectFit: "contain" }}
-      />
     </>
   );
 }
