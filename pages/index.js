f3adf9a18fdb1d9de79dfa245e299ae5c6b8b100
Lucas Alves <l.alves001@gmail.com>
2023-08-18 12:02:30 -0300

    Trying unoptimized image

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,12 @@
-import Image from "next/image";
-
 function Home() {
-  const isGitHubActions = process.env.GITHUB_ACTIONS || false;
-  const repository = isGitHubActions ? process.env.GITHUB_ACTIONS.replace(/.*?\//, "") : 'local';
-
   return (
     <>
-      <h2>Environment info</h2>
-      <p>Is GitHub: {`${isGitHubActions}`}</p>
-      <p>Repository: {repository}</p>
-      <div>
-        <Image
-          fill
-          src="/images/file-based-routing.png"
-          style={{ objectFit: "contain" }}
-        />
-      </div>
+      <h2>Home</h2>
+      <img
+        width="100%"
+        src="../images/file-based-routing.png"
+        style={{ objectFit: "contain" }}
+      />
     </>
   );
 }
