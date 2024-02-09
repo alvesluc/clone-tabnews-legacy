1c7d0489a1f3cc29ce45c5c62e021b3bdfa3f7d3
Lucas Alves <l.alves001@gmail.com>
2024-02-09 13:40:09 -0300

    fetching database connection from `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function handler(request, response) {
+import database from "../../../../infra/database.js";
+
+async function handler(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows);
+
   return response.status(200).json({ message: "funciona" });
 }
 
